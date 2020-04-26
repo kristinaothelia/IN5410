f5246bdbab77d7df0be1908230eceb48ad7cd2a2
Anna Eliassen <anna-e96@hotmail.com>
2020-04-26 23:01:48 +0200

    linreg_start

--- Assignment_2/readData.py
+++ Assignment_2/readData.py
@@ -26,11 +26,53 @@ def Data(filename='/TrainData.csv'):
     return Data
 
 
-TrainData = Data()
+# Data for training the model (x_train and y_train?):
+TrainData = Data(filename='/TrainData.csv')
+
+# Data for later prediction (X_test?): 
+WF_input  = Data(filename='/WeatherForecastInput.csv')
+
+# Solution used to calculate error of the predictions (y_test?)
+Solution  = Data(filename='/Solution.csv')
+
+# Does not seem like any of the datasets contain nan-values:
+
+#TrainData = TrainData.replace(r'^\s*$', np.nan, regex=True)
+#TrainData = pd.DataFrame.dropna(TrainData, axis=0, how='any')
+#WF_input  = WF_input.replace(r'^\s*$', np.nan, regex=True)
+#WF_input = pd.DataFrame.dropna(WF_input, axis=0, how='any')
+
+
+# 'Power' is the 'target' and the other columns are the 'features'
+# Only use the features for 10m, so dropping the 100m colums 
+
+TrainData.drop(columns=['U100', 'V100', 'WS100'], axis=1, inplace=True)
+WF_input.drop(columns =['U100', 'V100', 'WS100'], axis=1, inplace=True)
+
 print(TrainData)
 
-#TrainData.isnull().any()
+features = TrainData.loc[:, (TrainData.columns != 'POWER')].values
+target   = TrainData.loc[:, TrainData.columns == 'POWER'].values # Targets
 
+pred_features  = WF_input.loc[:, WF_input.columns != 'POWER'].values # Targets
+power_solution = Solution.loc[:, Solution.columns == 'POWER'].values # Targets
+
+print(features)
+print(target)
+
+from sklearn.linear_model  import LinearRegression
+from sklearn.metrics       import mean_squared_error
+
+linreg = LinearRegression()  
+linreg.fit(features, target) #training the algorithm
+
+y_pred = linreg.predict(pred_features)
+
+compare_values = pd.DataFrame({'Actual': power_solution.flatten(), 'Predicted': y_pred.flatten()})
+print(compare_values)
+
+mse = mean_squared_error(power_solution, y_pred)
+print(mse)
 
 #sc_feature = StandardScaler()
 #sc_target = StandardScaler()
